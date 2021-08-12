618c58dd61ebba15916953daa9a87c60e82fca1a
publina <mingzhangtech@foxmail.com>
2021-08-13 07:33:41 +0800

    优化后台路由

--- cloudfunctions/reg_cloud/comm/ccmini_router.js
+++ cloudfunctions/reg_cloud/comm/ccmini_router.js
@@ -19,48 +19,13 @@ module.exports = {
 	'test/test': 'TestController@test',   
 
 	'home/setup': 'HomeController@getSetup',  
-	'home/setup_all': 'HomeController@getSetupAll',  
- 
-	'info/list': 'InfoController@getInfoList',
-	'info/my_list': 'InfoController@getMyInfoList',
-	'info/insert': 'InfoController@insertInfo',
-	'info/my_detail': 'InfoController@getMyInfoDetail',
-	'info/edit': 'InfoController@editInfo',
-	'info/del': 'InfoController@delInfo', 
-	'info/view': 'InfoController@viewInfo',
-	'info/update_pic': 'InfoController@updateInfoPic',
- 
-	 
-
-	'album/list': 'AlbumController@getAlbumList',
-	'album/my_list': 'AlbumController@getMyAlbumList',
-	'album/insert': 'AlbumController@insertAlbum',
-	'album/my_detail': 'AlbumController@getMyAlbumDetail',
-	'album/edit': 'AlbumController@editAlbum',
-	'album/del': 'AlbumController@delAlbum', 
-	'album/view': 'AlbumController@viewAlbum',
-	'album/update_pic': 'AlbumController@updateAlbumPic',
-
-	'meet/list': 'MeetController@getMeetList',
-	'meet/my_list': 'MeetController@getMyMeetList',
-	'meet/insert': 'MeetController@insertMeet',
-	'meet/my_detail': 'MeetController@getMyMeetDetail',
-	'meet/edit': 'MeetController@editMeet',
-	'meet/del': 'MeetController@delMeet', 
-	'meet/view': 'MeetController@viewMeet',
-	'meet/update_pic': 'MeetController@updateMeetPic',
-	'meet/join_cancel': 'MeetController@cancelJoin',
-	'meet/join_insert': 'MeetController@insertJoin',
-	'meet/join_list': 'MeetController@getJoinList', 
+	'home/setup_all': 'HomeController@getSetupAll',   
   
 	'user/list': 'UserController@getUserList',
 	'user/detail': 'UserController@getUser',
 	'user/view': 'UserController@viewUser', 
 	'user/my_detail': 'UserController@getMyDetail',
- 
-	'my/info': 'MyController@getMyInfoList',  
-	'my/meet': 'MyController@getMyMeetList',  
-	'my/album': 'MyController@getMyAlbumList',  
+  
 	  
 	'passport/phone': 'PassportController@getPhone', 
 	'passport/reg': 'PassportController@register',
@@ -80,29 +45,7 @@ module.exports = {
 	'admin/user_list': 'AdminController@getUserList',
 	'admin/user_detail': 'AdminController@getUserDetail',
 	'admin/user_del': 'AdminController@delUser',
-	'admin/user_status': 'AdminController@statusUser',
-
-	'admin/info_list': 'AdminController@getInfoList',
-	'admin/info_detail': 'AdminController@getInfoDetail',
-	'admin/info_del': 'AdminController@delInfo',
-	'admin/info_sort': 'AdminController@sortInfo',
-	'admin/info_status': 'AdminController@statusInfo', 
-
-
-	'admin/join_list': 'AdminController@getJoinList',
-	'admin/meet_list': 'AdminController@getMeetList',
-	'admin/meet_detail': 'AdminController@getMeetDetail',
-	'admin/meet_del': 'AdminController@delMeet',
-	'admin/join_del': 'AdminController@delJoin',
-	'admin/meet_sort': 'AdminController@sortMeet',
-	'admin/meet_status': 'AdminController@statusMeet',
-
-	'admin/album_list': 'AdminController@getAlbumList',
-	'admin/album_detail': 'AdminController@getAlbumDetail',
-	'admin/album_del': 'AdminController@delAlbum',
-	'admin/album_sort': 'AdminController@sortAlbum',
-	'admin/album_status': 'AdminController@statusAlbum',
-  
+	'admin/user_status': 'AdminController@statusUser', 
 
 	'admin/news_list': 'AdminController@getNewsList',
 	'admin/news_insert': 'AdminController@insertNews',
