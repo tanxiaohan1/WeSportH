3a5c0de2e0ac953b8b5d8b7ecd5c79fd8a24f202
newboys111 <newboys111@foxmail.com>
2022-03-28 11:00:51 +0800

    CR

--- cloudfunctions/reg_cloud/index.js
+++
@@ -1,6 +0,0 @@
-const ccminiApplication = require('./framework/handler/ccmini_application.js');
-
-// 云函数入口函数
-exports.main = async (event, context) => {
-	return await ccminiApplication.ccminiApp(event, context);
-}
