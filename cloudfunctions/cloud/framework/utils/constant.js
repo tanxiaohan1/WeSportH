315062197289598f405db50e05f37e49293bfde2
tanxiaohan1 <tanxiaohan1@qq.com>
2023-02-06 05:25:11 +0800

    CR

--- cloudfunctions/cloud/framework/utils/constant.js
+++
@@ -1,8 +0,0 @@
- /**
- * Notes: 通用常量定义
- * Ver : CCMiniCloud Framework 2.0.1 ALL RIGHTS RESERVED BY cclinux@qq.com
- * Date: 2020-09-05 04:00:00 
- */
-module.exports = {
- 
-}
--- cloudfunctions/cloud/index.js
+++
@@ -1,6 +0,0 @@
-const application = require('./framework/core/application.js');
-
-// 云函数入口函数
-exports.main = async (event, context) => {
-	return await application.app(event, context);
-}
